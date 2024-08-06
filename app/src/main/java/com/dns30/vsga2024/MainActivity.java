452ffc8451e3bdd7f86d65a381ec43028a9eb786
Dani Setiawan <danisetiawan609@gmail.com>
2024-08-06 19:32:18 +0700

    Membuat fitur operasi baca dan hapus file

--- app/src/main/java/com/dns30/vsga2024/MainActivity.java
+++ app/src/main/java/com/dns30/vsga2024/MainActivity.java
@@ -1,6 +1,8 @@
 package com.dns30.vsga2024;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String FILENAME = "catatan.txt";
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,77 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        editText = findViewById(R.id.editText);
+
+        bacaFile();
+    }
+
+    private void buatFile() {
+        File file = new File(getFilesDir(), FILENAME);
+        FileOutputStream fos;
+        try {
+            file.createNewFile();
+            fos = new FileOutputStream(file, false);
+            fos.write(editText.getText().toString().getBytes());
+            fos.flush();
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private void ubahFile() {
+        String ubah = "Update isi Data File Text";
+        File file = new File(getFilesDir(), FILENAME);
+        FileOutputStream outputStream = null;
+        try {
+            file.createNewFile();
+            outputStream = new FileOutputStream(file, false);
+            outputStream.write(ubah.getBytes());
+            outputStream.flush();
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private void bacaFile() {
+        File file = new File(getFilesDir(), FILENAME);
+        if (file.exists()) {
+            StringBuilder text = new StringBuilder();
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String line = br.readLine();
+                while (line != null) {
+                    text.append(line);
+                    line = br.readLine();
+                }
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            editText.setText(text);
+        }
+        else {
+            editText.setText("");
+        }
+    }
+
+    private void hapusFile() {
+        File file = new File(getFilesDir(), FILENAME);
+        if (file.exists()) {
+            file.delete();
+            editText.setText("");
+        }
+    }
+
+    public void onClick(View view) {
+        if (view.getId() == R.id.buttonBuatFile)
+            buatFile();
+        else if (view.getId() == R.id.buttonBacaFile)
+            bacaFile();
+        else if (view.getId() == R.id.buttonUbahFile)
+            ubahFile();
+        else if (view.getId() == R.id.buttonHapusFile)
+            hapusFile();
     }
 }
